408fe10b51bd9000c32a38f19df8a5cc0a5624d5
vzmmmm777 <testesin79@gmail.com>
2025-05-30 16:13:43 +0000

    Delete nmap.py

--- nmap.py
+++
@@ -1,7 +0,0 @@
-print ("Checking Modules")
-print ("If a error use setup.py")
-print ("Thank you for use my tool")
-
-import os
-target_site = input("Enter Site Target:")
-os.system(f"sudo apt install nmap && clear && figlet nmap && nmap {target_site}")
