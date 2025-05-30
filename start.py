817d593b0c6d3ea8c339883f30243151e5aab235
vzmmmm777 <testesin79@gmail.com>
2025-05-30 01:24:43 +0000

    1.0.1 start.py

--- start.py
+++ start.py
@@ -30,21 +30,25 @@ print ("________________467935797error90347354289___________________")
 
 
 if result['usedtool'] == 'Nmap':
-    os.system("")
+    os.system("python3 nmap.py")
     os.system("python3 /home/guest/HackingTheTools/nmap.py")
     os.system("python3 /usr/share/HackingTheTools/nmap.py")
 elif result['usedtool'] == 'Hydra':
     os.system("python3 /home/guest/HackingTheTools/hydra.py")
     os.system("python3 /usr/share/HackingTheTools/hydra.py")
+    os.system("python3 hydra.py")
 elif result['usedtool'] == 'Nikto'
     os.system("python3 /home/guest/HackingTheTools/nikto.py")
     os.system("python3 /usr/share/HackingTheTools/nikto.py")
+    os.system("python3 nikto.py")
 elif result['usedtool'] == 'John The Ripper':
     os.system("python3 /home/guest/HackingTheTools/john.py")
     os.system("python3 /usr/share/HackingTheTools/john.py")
+    os.system("python3 john.py")
 elif result['usedtool'] == 'Doxxing':
     os.system("python3 /home/guest/HackingTheTools/doxxing.py")
     os.system("python3 /usr/share/HackingTheToolsmdoxxing.py")
+    os.system("python3 doxxing.py")
 
 
 
