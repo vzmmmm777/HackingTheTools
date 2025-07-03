cffad1cbb0b516429f70ea555d9c92d4972e0928
vzmmmm777 <testesin79@gmail.com>
2025-07-03 13:56:39 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ print ("Download Requirements")
 os.system("pip install selenium --break-system-packages")
 os.system("pip install driver --break-system-packages")
 os.system("pip install bs4 --break-system-packages")
-os.system("pip install prompt --break-system-packages")
+os.system("pip install InquirePy --break-system-packages")
 
 
 print ("All Packages Installed")
